63d31153dcb218e29d7573404c5196ea34b46aa1
David <37672966+Ieida@users.noreply.github.com>
2025-03-10 15:17:18 +0100

    Improvements

---
+++ elements/player/space_ship/viewmodel/player_space_ship_viewmodel.gd
@@ -0,0 +1,4 @@
+class_name PlayerSpaceShipViewmodel extends Camera3D
+
+
+@export var space_ship: SpaceShip
--- elements/space_ship/space_ship.gd
+++ elements/space_ship/space_ship.gd
@@ -8,7 +8,6 @@ class_name SpaceShip extends CharacterBody3D
 @export var slow_down_force: float = 4
 @export var torque_x: float = 2
 @export var torque_y: float = 2
-@onready var crosshair: Crosshair = $UI/Control/Crosshair
 var input: Vector3
 var angular_input: Vector3
 
@@ -19,8 +18,10 @@ func _physics_process(delta: float) -> void:
 		angular_input = Vector3.ZERO
 		inp.x *= torque_x
 		inp.y *= torque_y
-		rotate(global_basis.y, deg_to_rad(-inp.x))
-		rotate(global_basis.x, deg_to_rad(-inp.y))
+		var up = global_basis * Vector3.UP
+		var right = global_basis * Vector3.RIGHT
+		rotate(up, deg_to_rad(-inp.x))
+		rotate(right, deg_to_rad(-inp.y))
 	global_rotation.z = move_toward(global_rotation.z, 0, delta)
 	if input:
 		var inp = input
