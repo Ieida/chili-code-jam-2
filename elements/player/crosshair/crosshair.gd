52dd614d0d6d722f984311c956237fc45fd55118
David <37672966+Ieida@users.noreply.github.com>
2025-03-10 15:43:29 +0100

    Handling improvements

--- elements/player/crosshair/crosshair.gd
+++ elements/player/crosshair/crosshair.gd
@@ -16,9 +16,12 @@ class_name Crosshair extends Control
 		queue_redraw()
 @export_range(0, 1, 0.01) var max_radius: float = 0.75
 @export var joy_sensitivity: float = 16.0
+@export var back_to_center: bool
+@export var back_to_center_speed: float = 4
 var edge_distance: float
 var edge_distance_x: float
 var edge_distance_y: float
+var recieved_input: bool
 
 
 func _draw() -> void:
@@ -28,24 +31,32 @@ func _draw() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		position += event.relative
+		recieved_input = true
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if not Engine.is_editor_hint():
 		var pos = position
 		# Add joy input
 		var jih = Input.get_axis(&"look_left", &"look_right")
 		var jiv = Input.get_axis(&"look_up", &"look_down")
+		# Modify input
+		jih *= absf(jih) * 2.0
+		jiv *= absf(jiv) * 2.0
+		if not recieved_input and (not is_zero_approx(jih) or not is_zero_approx(jiv)):
+			recieved_input = true
 		pos += Vector2(jih * joy_sensitivity, jiv * joy_sensitivity)
 		var vs = get_viewport_rect().size
 		var hvs = vs / 2.0
 		var ctr = vs / 2.0
+		var dst = ctr.distance_to(pos)
+		if not recieved_input and back_to_center:
+			pos = pos.move_toward(ctr, dst * back_to_center_speed * delta)
 		# Find radius
 		var rds = 0.0
 		if vs.x < vs.y: rds = hvs.x * max_radius
 		else: rds = hvs.y * max_radius
 		# Constrain to radius
-		var dst = ctr.distance_to(pos)
 		if dst > rds: pos = ctr + (ctr.direction_to(pos) * rds)
 		position = pos
 		# Calculate edge distance
@@ -53,3 +64,5 @@ func _process(_delta: float) -> void:
 		edge_distance = ndst.length() / rds
 		edge_distance_x = ndst.x / rds
 		edge_distance_y = ndst.y / rds
+		
+		if recieved_input: recieved_input = false
--- elements/player/space_ship/controller/player_space_ship_controller.gd
+++ elements/player/space_ship/controller/player_space_ship_controller.gd
@@ -1,6 +1,7 @@
 class_name PlayerSpaceShipController extends Node
 
 
+@export_range(0, 1, 0.01) var look_deadzone: float = 0.4
 @onready var viewmodel: PlayerSpaceShipViewmodel = get_parent() as PlayerSpaceShipViewmodel
 @onready var crosshair: Crosshair = %Crosshair
 
@@ -11,7 +12,7 @@ func _physics_process(_delta: float) -> void:
 	var c_in := Input.get_axis(&"front", &"back")
 	var inp_v := Vector3(h_in, v_in, c_in)
 	viewmodel.space_ship.input = inp_v
-	if crosshair.edge_distance > 0.2:
+	if crosshair.edge_distance > look_deadzone:
 		var h_ain = crosshair.edge_distance_x
 		var v_ain = crosshair.edge_distance_y
 		viewmodel.space_ship.angular_input = Vector3(h_ain, v_ain, 0)
