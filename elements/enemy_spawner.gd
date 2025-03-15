eae16ae14581522626c1c8c862163807d7719f7e
David <37672966+Ieida@users.noreply.github.com>
2025-03-15 20:54:57 +0100

    improvements

--- elements/enemy_spawner.gd
+++ elements/enemy_spawner.gd
@@ -1,6 +1,9 @@
 class_name EnemySpawner extends Spawner
 
 
+signal wave_started
+
+
 @export var normal_scene: PackedScene
 @export var normal_wave_multiplier: float = 1.2
 @export var normals_per_wave: int = 20
@@ -11,12 +14,19 @@ var spawns: int
 var wave: int
 
 
+func _ready() -> void:
+	wave += 1
+	wave_started.emit()
+
+
 func spawn():
 	spawn_scene(normal_scene)
 	spawns += 1
-	var w = wave + 1
+	var w = wave
 	if spawns == roundf(float(normals_per_wave) * (w * normal_wave_multiplier)):
 		var ec = roundf(float(elites_per_wave) * elite_wave_multiplier * wave)
 		for ei in int(ec):
 			spawn_scene(elite_scene)
 		wave += 1
+		print("wave started")
+		wave_started.emit()
